adfa056e827fe246273b3e5786cb8b006ffae289
Chrystiangit <chrystianmiguel@hotmail.com>
2026-08-21 16:43:44 -0400

    inicialiação do projeto

---
+++ src/main/java/com/chrystian/testetecnicomoduloproduto/TesteTecnicoModuloProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.chrystian.testetecnicomoduloproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoModuloProdutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteTecnicoModuloProdutoApplication.class, args);
+    }
+
+}
